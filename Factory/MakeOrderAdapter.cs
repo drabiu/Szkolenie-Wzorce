5febf2219b6f08b480bda7b532fabca649075e4b
drabiu <drabiozo@o2.pl>
2017-10-21 17:00:31 +0200

    sdgre43

--- Factory/MakeOrderAdapter.cs
+++ Factory/MakeOrderAdapter.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using Factory.ludovisko;
 
 namespace Jedzenie
 {
     public class MakeOrderAdapter : IMakeOrder
     {
         public IEnumerable<IMenuItem> ShowOrderDetails(IBasket basket)
+        {
+            var api = new LudoviskoAPIClient("a", "b");
+            var order = Convert(basket);
+            api.SendOrder(order);
+
+            return basket.GetItems();
+        }
+
+        private IList<Tuple<string, int>> Convert(IBasket basket)
         {
             throw new NotImplementedException();
         }
---
+++ Factory/ludovisko/LudoviskoAPIClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.ludovisko
+{
+    public class LudoviskoAPIClient
+    {
+        public LudoviskoAPIClient(string clientId, string clientKey)
+        {
+            
+        }
+
+        public void SendOrder(IList<Tuple<string, int>> items)
+        {
+            
+        }
+    }
+}
