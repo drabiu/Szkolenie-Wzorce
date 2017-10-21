ac5cb1202e5c0e4fbabf11d5ea1d45f183d73e25
drabiu <drabiozo@o2.pl>
2017-10-21 11:42:22 +0200

    dfd
    ffdd

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _lock = new Object();
+        private Singleton()
+        {
+
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Singleton/SingletonTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Singleton
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void Singleton_has_only_one_instance()
+        {
+            var obj1 = Singleton.Instance;
+            var obj2 = Singleton.Instance;
+
+            Assert.Equal(obj1.GetHashCode(), obj2.GetHashCode());
+        }
+    }
+
+}
